fb1f867b23edd0adecaec50c3774d68fbd4eb24d
Billy O'Connor <oconnor.iam@gmail.com>
2020-03-03 09:01:44 +0000

    First commit via CLI

---
+++ scripts/forecast.js
@@ -0,0 +1,24 @@
+const key = 'mCNL1ZoHqYZZVJDxDaz8tNzggrYDpEtv';
+
+// get weather information
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    return data[0];
+};
+
+// get City Information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
